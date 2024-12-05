32f95f5ec4402fe7fa22a578c0b1f54cd88b6fdb
Erik van der Zwan <E.van.der.Zwan@chipsoft.com>
2024-12-05 15:16:38 +0100

    added day 3

---
+++ AdventOfCode2024/Day3.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode2024;
+
+static partial class Day3
+{
+    public static void ExecutePart1()
+    {
+        var instructions = ResourceLoader.LoadText("Day3.txt");
+
+        var total = MultiplyRegex().Matches(instructions).Sum(Multiply);
+        Console.WriteLine(total);
+    }
+
+    public static void ExecutePart2()
+    {
+        var instructions = ResourceLoader.LoadText("Day3.txt");
+        var parts = instructions.Split("do()", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+
+        var total = parts.Sum(p => MultiplyRegex().Matches(p.Contains("don't()") ? p[..p.IndexOf("don't()")] : p).Sum(Multiply));
+        Console.WriteLine(total);
+    }
+
+    static int Multiply(Match match) => int.Parse(match.Groups[1].Value) * int.Parse(match.Groups[2].Value);
+
+    [GeneratedRegex(@"mul\x28(\d{1,3}),(\d{1,3})\x29")]
+    private static partial Regex MultiplyRegex();
+}
--- AdventOfCode2024/Program.cs
+++ AdventOfCode2024/Program.cs
@@ -1,4 +1,4 @@
 ﻿using AdventOfCode2024;
 
-Day2.ExecutePart1();
-Day2.ExecutePart2();
+Day3.ExecutePart1();
+Day3.ExecutePart2();
