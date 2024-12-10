8736c91c9682c70f29c33323611c437f683e0d98
Erik van der Zwan <E.van.der.Zwan@chipsoft.com>
2024-12-10 17:04:47 +0100

    Day 10 shortened

--- AdventOfCode2024/Day10.cs
+++ AdventOfCode2024/Day10.cs
@@ -5,22 +5,18 @@ class Day10() : Day<int>(36, 81)
     protected override int ExecutePart1(string input, bool example)
     {
         var matrix = input.Split(Environment.NewLine).Select(r => r.Select(c => c - '0').ToArray()).ToArray();
-        var size = matrix.Length;
-
-        return Enumerable.Range(0, size).SelectMany(y => Enumerable.Range(0, size).Select(x => GetTrailheads(matrix, size, x, y, 0, 9).Distinct().Count())).Sum();
+        return Enumerable.Range(0, matrix.Length).SelectMany(y => Enumerable.Range(0, matrix[y].Length).Select(x => GetTrailheads(matrix, x, y, 0, 9).Distinct().Count())).Sum();
     }
 
     protected override int ExecutePart2(string input, bool example)
     {
         var matrix = input.Split(Environment.NewLine).Select(r => r.Select(c => c - '0').ToArray()).ToArray();
-        var size = matrix.Length;
-
-        return Enumerable.Range(0, size).SelectMany(y => Enumerable.Range(0, size).Select(x => GetTrailheads(matrix, size, x, y, 0, 9).Length)).Sum();
+        return Enumerable.Range(0, matrix.Length).SelectMany(y => Enumerable.Range(0, matrix[y].Length).Select(x => GetTrailheads(matrix, x, y, 0, 9).Length)).Sum();
     }
 
-    static (int X, int Y)[] GetTrailheads(int[][] matrix, int size, int x, int y, int current, int target)
+    static (int X, int Y)[] GetTrailheads(int[][] matrix, int x, int y, int current, int target)
     {
-        if (x < 0 || x >= size || y < 0 || y >= size || matrix[y][x] != current)
+        if (y < 0 || y >= matrix.Length || x < 0 || x >= matrix[y].Length || matrix[y][x] != current)
         {
             return [];
         }
@@ -30,9 +26,9 @@ class Day10() : Day<int>(36, 81)
             return [(x, y)];
         }
 
-        return [..GetTrailheads(matrix, size, x - 1, y, current + 1, target),
-                ..GetTrailheads(matrix, size, x, y - 1, current + 1, target),
-                ..GetTrailheads(matrix, size, x + 1, y, current + 1, target),
-                ..GetTrailheads(matrix, size, x, y + 1, current + 1, target)];
+        return [..GetTrailheads(matrix, x - 1, y, current + 1, target),
+                ..GetTrailheads(matrix, x, y - 1, current + 1, target),
+                ..GetTrailheads(matrix, x + 1, y, current + 1, target),
+                ..GetTrailheads(matrix, x, y + 1, current + 1, target)];
     }
 }
