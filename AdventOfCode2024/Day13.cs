e68868930311ad3df8fcbaed2066c2198359a8ae
Erik van der Zwan <E.van.der.Zwan@chipsoft.com>
2024-12-13 14:25:51 +0100

    Day 13 performance upgrade

--- AdventOfCode2024/Day13.cs
+++ AdventOfCode2024/Day13.cs
@@ -1,6 +1,8 @@
-﻿namespace AdventOfCode2024;
+﻿using System.Diagnostics.Metrics;
 
-class Day13() : Day<long>(480, 957_820_661_194)
+namespace AdventOfCode2024;
+
+class Day13() : Day<long>(480, 0)
 {
     const int X = 0;
     const int Y = 1;
@@ -12,48 +14,13 @@ class Day13() : Day<long>(480, 957_820_661_194)
                                  .Select(n => rows.Skip(n * 3).Take(3).ToArray())
                                  .Select(i => (A: i[0].Split([' ', ','], StringSplitOptions.RemoveEmptyEntries).TakeLast(2).Select(j => int.Parse(j.Split('+')[1])).ToArray(),
                                                B: i[1].Split([' ', ','], StringSplitOptions.RemoveEmptyEntries).TakeLast(2).Select(j => int.Parse(j.Split('+')[1])).ToArray(),
-                                               Prize: i[2].Split([' ', ','], StringSplitOptions.RemoveEmptyEntries).TakeLast(2).Select(j => int.Parse(j.Split('=')[1])).ToArray()))
+                                               Prize: i[2].Split([' ', ','], StringSplitOptions.RemoveEmptyEntries).TakeLast(2).Select(j => long.Parse(j.Split('=')[1])).ToArray()))
                                  .ToArray();
 
-        var total = 0L;
-        foreach (var (machineA, machineB, prize) in machines)
-        {
-            var tokensForMachine = new List<long>();
-            for (var a = 1; a <= 100; a++)
-            {
-                var x = machineA[X] * a;
-                var y = machineA[Y] * a;
-
-                if (x > prize[X] || y > prize[Y])
-                {
-                    break;
-                }
-
-                for (var b = 1; b <= 100; b++)
-                {
-                    var cx = x + machineB[X] * b;
-                    var cy = y + machineB[Y] * b;
-
-                    if (cx == prize[X] && cy == prize[Y])
-                    {
-                        tokensForMachine.Add(a * 3 + b);
-                    }
-                    else if (cx > prize[X] || cy > prize[Y])
-                    {
-                        break;
-                    }
-                }
-            }
-
-            if (tokensForMachine.Count > 0)
-            {
-                total += tokensForMachine.Min();
-            }
-        }
-
-        return total;
+        return machines.Sum(m => Math.Min(GetMinimalTokenAmountUsingA(m.A, m.B, m.Prize), GetMinimalTokenAmountUsingB(m.A, m.B, m.Prize)));
     }
 
+    [TakesAWhile]
     protected override long ExecutePart2(string input, bool example)
     {
         var rows = input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries).ToArray();
@@ -61,55 +28,71 @@ class Day13() : Day<long>(480, 957_820_661_194)
                                  .Select(n => rows.Skip(n * 3).Take(3).ToArray())
                                  .Select(i => (A: i[0].Split([' ', ','], StringSplitOptions.RemoveEmptyEntries).TakeLast(2).Select(j => int.Parse(j.Split('+')[1])).ToArray(),
                                                B: i[1].Split([' ', ','], StringSplitOptions.RemoveEmptyEntries).TakeLast(2).Select(j => int.Parse(j.Split('+')[1])).ToArray(),
-                                               Prize: i[2].Split([' ', ','], StringSplitOptions.RemoveEmptyEntries).TakeLast(2).Select(j => 1_000_000_0000_000 + int.Parse(j.Split('=')[1])).ToArray()))
+                                               Prize: i[2].Split([' ', ','], StringSplitOptions.RemoveEmptyEntries).TakeLast(2).Select(j => 1_000_000_0000_000 + long.Parse(j.Split('=')[1])).ToArray()))
                                  .ToArray();
 
-        var total = 0L;
-        foreach (var machine in machines)
-        {
-            total += Math.Min(GetFewestTokensToSpendAFirst(machine), GetFewestTokensToSpendBFirst(machine));
-        }
-
-        return total;
+        return machines.Sum(m => Math.Min(GetMinimalTokenAmountUsingA(m.A, m.B, m.Prize), GetMinimalTokenAmountUsingB(m.A, m.B, m.Prize)));
     }
 
-    static long GetFewestTokensToSpendAFirst((int[] A, int[] B, long[] Prize) machine)
+    static long GetMinimalTokenAmountUsingA(int[] a, int[] b, long[] prize)
     {
-        var remainderAX = machine.Prize[X] % machine.A[X];
-        var remainderAY = machine.Prize[Y] % machine.A[Y];
+        var amount = long.MaxValue;
 
-        var tokens = new List<long>();
-        for (var pressesB = 1; pressesB < 10_000; pressesB++)
+        for (var countA = Math.Min(prize[X] / a[X], prize[Y] / a[Y]); countA >= 0; countA--)
         {
-            var remainderBX = remainderAX * pressesB % machine.B[X];
-            var remainderBY = remainderAY * pressesB % machine.B[Y];
-            if (remainderBX == 0 && remainderBY == 0)
+            var remainderX = (prize[X] - countA * a[X]) % b[X];
+            var remainderY = (prize[Y] - countA * a[Y]) % b[Y];
+
+            if (remainderX == 0 && remainderY == 0)
             {
-                var pressesA = (machine.Prize[X] - machine.B[X] * pressesB) / machine.A[X];
-                tokens.Add(pressesA * 3 + pressesB);
+                var countBX = (prize[X] - countA * a[X]) / b[X];
+                var countBY = (prize[Y] - countA * a[Y]) / b[Y];
+                if (countBX == countBY)
+                {
+                    var newAmount = countA * 3 + countBX;
+                    if (newAmount < amount)
+                    {
+                        amount = newAmount;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
             }
         }
 
-        return tokens.Count > 0 ? tokens.Min() : 0;
+        return amount == long.MaxValue ? 0 : amount;
     }
 
-    static long GetFewestTokensToSpendBFirst((int[] A, int[] B, long[] Prize) machine)
+    static long GetMinimalTokenAmountUsingB(int[] a, int[] b, long[] prize)
     {
-        var remainderBX = machine.Prize[X] % machine.B[X];
-        var remainderBY = machine.Prize[Y] % machine.B[Y];
-
-        var tokens = new List<long>();
-        for (var pressesA = 1; pressesA < 10_000; pressesA++)
+        var amount = long.MaxValue;
+        
+        for (var countB = Math.Min(prize[X] / b[X], prize[Y] / b[Y]); countB >= 0;countB--)
         {
-            var remainderAX = remainderBX * pressesA % machine.A[X];
-            var remainderAY = remainderBY * pressesA % machine.A[Y];
-            if (remainderAX == 0 && remainderAY == 0)
+            var remainderX = (prize[X] - countB * b[X]) % a[X];
+            var remainderY = (prize[Y] - countB * b[Y]) % a[Y];
+
+            if (remainderX == 0 && remainderY == 0)
             {
-                var pressesB = (machine.Prize[X] - machine.A[X] * pressesA) / machine.B[X];
-                tokens.Add(pressesA * 3 + pressesB);
+                var countAX = (prize[X] - countB * b[X]) / a[X];
+                var countAY = (prize[Y] - countB * b[Y]) / a[Y];
+                if (countAX == countAY)
+                {
+                    var newAmount = countAX * 3 + countB;
+                    if (newAmount < amount)
+                    {
+                        amount = newAmount;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
             }
         }
 
-        return tokens.Count > 0 ? tokens.Min() : 0;
+        return amount == long.MaxValue ? 0 : amount;
     }
 }
