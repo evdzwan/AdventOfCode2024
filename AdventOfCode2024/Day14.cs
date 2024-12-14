97bb9c3224fd06dcc6ab5747b8019cd98ec659af
evanderzwan <evanderzwan@gmail.com>
2024-12-14 20:04:25 +0100

    Added day 12 part 2

--- AdventOfCode2024/Day14.cs
+++ AdventOfCode2024/Day14.cs
@@ -1,6 +1,6 @@
 ﻿namespace AdventOfCode2024;
 
-class Day14() : Day<int>(12, 1)
+class Day14() : Day<int>(12, 0)
 {
     const int POSITION = 0;
     const int VELOCITY = 1;
@@ -26,9 +26,31 @@ class Day14() : Day<int>(12, 1)
         return GetSafetyFactor(robots, width, height);
     }
 
+    [TakesAWhile]
     protected override int ExecutePart2(string input, bool example)
     {
-        return 0;
+        if (example)
+        {
+            return 0;
+        }
+
+        var width = example ? 11 : 101;
+        var height = example ? 7 : 103;
+
+        var robots = GetRobots(input);
+        for (var seconds = 0; ; seconds++)
+        {
+            if (IsChristmasTree(robots, width, height))
+            {
+                return seconds;
+            }
+
+            foreach (var robot in robots)
+            {
+                robot[POSITION][X] = (robot[POSITION][X] + robot[VELOCITY][X] + width) % width;
+                robot[POSITION][Y] = (robot[POSITION][Y] + robot[VELOCITY][Y] + height) % height;
+            }
+        }
     }
 
     static int[][][] GetRobots(string input)
@@ -46,5 +68,47 @@ class Day14() : Day<int>(12, 1)
                robots.Count(r => r[POSITION][X] < ((width - 1) / 2) && r[POSITION][Y] >= ((height + 1) / 2)) *
                robots.Count(r => r[POSITION][X] >= ((width + 1) / 2) && r[POSITION][Y] < ((height - 1) / 2)) *
                robots.Count(r => r[POSITION][X] >= ((width + 1) / 2) && r[POSITION][Y] >= ((height + 1) / 2));
+    }
+
+    static bool IsChristmasTree(int[][][] robots, int width, int height)
+    {
+        var lookup = robots.ToLookup(r => (X: r[POSITION][X], Y: r[POSITION][Y]));
+        foreach (var group in lookup)
+        {
+            var top = group.Key;
+            if (ContainsRow(top with { Y = top.Y + 1 }, 3) &&
+                ContainsRow(top with { Y = top.Y + 2 }, 5) &&
+                ContainsRow(top with { Y = top.Y + 3 }, 7) &&
+                ContainsRow(top with { Y = top.Y + 4 }, 9) &&
+                ContainsRow(top with { Y = top.Y + 5 }, 5) &&
+                ContainsRow(top with { Y = top.Y + 6 }, 7) &&
+                ContainsRow(top with { Y = top.Y + 7 }, 9) &&
+                ContainsRow(top with { Y = top.Y + 8 }, 11) &&
+                ContainsRow(top with { Y = top.Y + 9 }, 13) &&
+                ContainsRow(top with { Y = top.Y + 10 }, 9) &&
+                ContainsRow(top with { Y = top.Y + 11 }, 11) &&
+                ContainsRow(top with { Y = top.Y + 12 }, 13) &&
+                ContainsRow(top with { Y = top.Y + 13 }, 15) &&
+                ContainsRow(top with { Y = top.Y + 14 }, 17) &&
+                ContainsRow(top with { Y = top.Y + 15 }, 13) &&
+                ContainsRow(top with { Y = top.Y + 16 }, 15) &&
+                ContainsRow(top with { Y = top.Y + 17 }, 17) &&
+                ContainsRow(top with { Y = top.Y + 18 }, 19) &&
+                ContainsRow(top with { Y = top.Y + 19 }, 21) &&
+                ContainsRow(top with { Y = top.Y + 20 }, 3) &&
+                ContainsRow(top with { Y = top.Y + 21 }, 3) &&
+                ContainsRow(top with { Y = top.Y + 22 }, 3))
+            {
+                return true;
+            }
+        }
+
+        return false;
+
+        bool ContainsRow((int X, int Y) coordinate, int size)
+        {
+            return Enumerable.Range(coordinate.X - (size - 1) / 2, size)
+                             .All(x => lookup.Contains(coordinate with { X = x }));
+        }
     }
 }
