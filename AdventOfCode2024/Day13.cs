b6b2015c488037f7a4f706f1128de1a9ca00aa38
evanderzwan <evanderzwan@gmail.com>
2024-12-14 17:34:59 +0100

    Day 13 performance upgrade

--- AdventOfCode2024/Day13.cs
+++ AdventOfCode2024/Day13.cs
@@ -1,104 +1,69 @@
 ﻿namespace AdventOfCode2024;
 
-class Day13() : Day<long>(480, 0)
+class Day13() : Day<long>(480, 875_318_608_908)
 {
+    const int A = 0;
+    const int B = 1;
+    const int Prize = 2;
+
     const int X = 0;
     const int Y = 1;
 
     protected override long ExecutePart1(string input, bool example)
     {
-        var rows = input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries).ToArray();
-        var machines = Enumerable.Range(0, rows.Length / 3)
-                                 .Select(n => rows.Skip(n * 3).Take(3).ToArray())
-                                 .Select(i => (A: i[0].Split([' ', ','], StringSplitOptions.RemoveEmptyEntries).TakeLast(2).Select(j => int.Parse(j.Split('+')[1])).ToArray(),
-                                               B: i[1].Split([' ', ','], StringSplitOptions.RemoveEmptyEntries).TakeLast(2).Select(j => int.Parse(j.Split('+')[1])).ToArray(),
-                                               Prize: i[2].Split([' ', ','], StringSplitOptions.RemoveEmptyEntries).TakeLast(2).Select(j => long.Parse(j.Split('=')[1])).ToArray()))
-                                 .ToArray();
-
-        return machines.Sum(m =>
+        var total = 0L;
+        foreach (var machine in GetMachines(input))
         {
-            var amount = Math.Min(GetMinimalTokenAmountUsingA(m.A, m.B, m.Prize), GetMinimalTokenAmountUsingB(m.A, m.B, m.Prize));
-            return amount == long.MaxValue ? 0 : amount;
-        });
+            var d = machine[A][X] * machine[B][Y] - machine[A][Y] * machine[B][X];
+            if (d == 0)
+            {
+                continue;
+            }
+
+            var dx = machine[Prize][X] * machine[B][Y] - machine[Prize][Y] * machine[B][X];
+            var dy = machine[A][X] * machine[Prize][Y] - machine[A][Y] * machine[Prize][X];
+            if (dx % d == 0 && dx % d == 0)
+            {
+                total += dx / d * 3 + dy / d;
+            }
+        }
+
+        return total;
     }
 
-    [TakesAWhile]
     protected override long ExecutePart2(string input, bool example)
     {
-        var rows = input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries).ToArray();
-        var machines = Enumerable.Range(0, rows.Length / 3)
-                                 .Select(n => rows.Skip(n * 3).Take(3).ToArray())
-                                 .Select(i => (A: i[0].Split([' ', ','], StringSplitOptions.RemoveEmptyEntries).TakeLast(2).Select(j => int.Parse(j.Split('+')[1])).ToArray(),
-                                               B: i[1].Split([' ', ','], StringSplitOptions.RemoveEmptyEntries).TakeLast(2).Select(j => int.Parse(j.Split('+')[1])).ToArray(),
-                                               Prize: i[2].Split([' ', ','], StringSplitOptions.RemoveEmptyEntries).TakeLast(2).Select(j => 1_000_000_0000_000 + long.Parse(j.Split('=')[1])).ToArray()))
-                                 .ToArray();
+        const long Offset = 10_000_000_000_000L;
 
-        return machines.Sum(m =>
+        var total = 0L;
+        foreach (var machine in GetMachines(input))
         {
-            var amount = Math.Min(GetMinimalTokenAmountUsingA(m.A, m.B, m.Prize), GetMinimalTokenAmountUsingB(m.A, m.B, m.Prize));
-            return amount == long.MaxValue ? 0 : amount;
-        });
-    }
-
-    static long GetMinimalTokenAmountUsingA(int[] a, int[] b, long[] prize)
-    {
-        var amount = long.MaxValue;
-
-        for (var countA = Math.Min(prize[X] / a[X], prize[Y] / a[Y]); countA >= 0; countA--)
-        {
-            var remainderX = (prize[X] - countA * a[X]) % b[X];
-            var remainderY = (prize[Y] - countA * a[Y]) % b[Y];
-
-            if (remainderX == 0 && remainderY == 0)
+            var d = machine[A][X] * machine[B][Y] - machine[A][Y] * machine[B][X];
+            if (d == 0)
             {
-                var countBX = (prize[X] - countA * a[X]) / b[X];
-                var countBY = (prize[Y] - countA * a[Y]) / b[Y];
-                if (countBX == countBY)
-                {
-                    var newAmount = countA * 3 + countBX;
-                    if (newAmount < amount)
-                    {
-                        amount = newAmount;
-                    }
-                    else
-                    {
-                        break;
-                    }
-                }
+                continue;
+            }
+
+            var dx = (Offset + machine[Prize][X]) * machine[B][Y] - (Offset + machine[Prize][Y]) * machine[B][X];
+            var dy = machine[A][X] * (Offset + machine[Prize][Y]) - machine[A][Y] * (Offset + machine[Prize][X]);
+            if (dx % d == 0 && dx % d == 0)
+            {
+                total += dx / d * 3 + dy / d;
             }
         }
 
-        return amount;
+        return total;
     }
 
-    static long GetMinimalTokenAmountUsingB(int[] a, int[] b, long[] prize)
+    static int[][][] GetMachines(string input)
     {
-        var amount = long.MaxValue;
-
-        for (var countB = Math.Min(prize[X] / b[X], prize[Y] / b[Y]); countB >= 0; countB--)
-        {
-            var remainderX = (prize[X] - countB * b[X]) % a[X];
-            var remainderY = (prize[Y] - countB * b[Y]) % a[Y];
-
-            if (remainderX == 0 && remainderY == 0)
-            {
-                var countAX = (prize[X] - countB * b[X]) / a[X];
-                var countAY = (prize[Y] - countB * b[Y]) / a[Y];
-                if (countAX == countAY)
-                {
-                    var newAmount = countAX * 3 + countB;
-                    if (newAmount < amount)
-                    {
-                        amount = newAmount;
-                    }
-                    else
-                    {
-                        break;
-                    }
-                }
-            }
-        }
-
-        return amount;
+        return input.Split($"{Environment.NewLine}{Environment.NewLine}")
+                    .Select(m => m.Split(Environment.NewLine)
+                                  .Select(i => i.Split([' ', ','], StringSplitOptions.RemoveEmptyEntries)
+                                                .TakeLast(2)
+                                                .Select(c => int.Parse(c.Split(['+', '='])[1]))
+                                                .ToArray())
+                                  .ToArray())
+                    .ToArray();
     }
 }
